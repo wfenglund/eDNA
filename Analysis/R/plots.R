36c3246ae7ec99018c2e9ff3928cc01a13536560
Thomas Källman <thomas.kallman@nrm.se>
2022-03-08 18:43:11 +0100

    Add plot functions to package

---
+++ Analysis/R/plots.R
@@ -0,0 +1,46 @@
+#' Barplots with fractions
+#'
+#' Creates a simple horizontal barplot showing the fraction of the
+#' total from each species.
+#'
+#' @param data a dataframe with count data from each species on rows
+#' @param species a vector with names of the species in the same order
+#' as the appear in the data
+#' @param col of colors to use in legend and plot.
+#' @export
+#'
+#' @examples
+#' data <- data.frame(A = 1:3, B = c(1,4,0), C = c(2, 3, 7))
+#' species <- c("Abramis brama", "Salmo trutta", "Salmo salar")
+#' col <- c("blue", "orange", "grey")
+#' fractionPlot(data = data, species = species, col = col)
+
+FractionPlot <- function(data, species, col) {
+  par(mar=c(4.1, 9.1, 4.1, 12.1), xpd=TRUE, las = 1, srt = 0)
+  props <- prop.table(as.matrix(data), margin=2) * 100
+  barplot(props, col = col, horiz = T)
+  legend("topright", legend = species, bty ="n", fill = col, inset=c(-0.3,.01))
+}
+
+#' Create a simple plot showing count data from individual samples
+#'
+#' @param data dataframe with count data
+#' @param sample name of sample to be plotted
+#' @param title title to be put on the plot
+#' @param species vector of species names
+#' @import forcats ggplot2 
+#' @export 
+#' 
+
+SeqPlot <- function(data, sample, title, species) {
+    Fs <- data[data[,sample]>0,]
+    speciesFactors <- forcats::fct_reorder(as.factor(species))
+    P1 <- ggplot(Fs) + 
+    geom_point(aes_string(y = sample, x = speciesFactors, Fs[,sample])) + 
+    xlab("") + ylab("") + 
+    scale_y_log10() + 
+    theme_bw() + 
+    coord_flip() + 
+    labs(title = title)
+    P1
+}

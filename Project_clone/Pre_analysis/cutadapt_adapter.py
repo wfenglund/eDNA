e5d29a20b582d85d6b2c6bdc9c175fbb39c7541e
wfenglund <w.f.englund@gmail.com>
2022-08-26 14:28:29 +0200

    fixed cutadapt_adapter input- and output-location and option to run reverse

--- Project_clone/Pre_analysis/cutadapt_adapter.py
+++ Project_clone/Pre_analysis/cutadapt_adapter.py
@@ -9,7 +9,9 @@ primer_r = "AGGCATRTGCCAAACATGA" # Reverse primer
 ###
 f_format = "novo" #scilife or novo
 anchored_flag = "yes" #yes or no
-reads_folder = "../Data"
+reverse_flag = "yes" #yes or no
+reads_folder = "../Raw_data" # Location of raw fastq-file
+write_folder = "../Filtered_data" # Location where trimmed reads will be written
 
 ###Main:
 
@@ -46,7 +48,8 @@ if test1 + test2 != 2:
 for file in os.listdir(reads_folder): #For every file in folder containing reads
     if file.endswith(file_end): #Check if file is a .fastq-read-file
         sample = file.replace(file_end, "") #Extract sample name
-        current_list = list([sample + "_outFwd_1.fastq.gz", sample + "_outFwd_2.fastq.gz", sample + "_outRev_1.fastq.gz", sample + "_outRev_2.fastq.gz"]) #Create filtering names and store in a list
+        current_list = list([write_folder + "/" + sample + "_outFwd_1.fastq.gz", write_folder + "/" + sample + "_outFwd_2.fastq.gz", write_folder + "/" + sample + "_outRev_1.fastq.gz", write_folder + "/" + sample + "_outRev_2.fastq.gz"]) #Create filtering names and store in a list
         
         subprocess.run([f'cd {reads_folder} ; cutadapt -j 0 --max-n=0 --discard-untrimmed -g {regular_primer} -G {reverse_primer} -o {current_list[0]} -p {current_list[1]} {file} {file.replace(repl_from, repl_to)}'], shell=True) #Run cutadapt for regular direction
-        subprocess.run([f'cd {reads_folder} ; cutadapt -j 0 --max-n=0 --discard-untrimmed -g {regular_primer} -G {reverse_primer} -o {current_list[2]} -p {current_list[3]} {file.replace(repl_from, repl_to)} {file}'], shell=True) #Run cutadapt in reverse direction
+        if reverse_flag == "yes":
+            subprocess.run([f'cd {reads_folder} ; cutadapt -j 0 --max-n=0 --discard-untrimmed -g {regular_primer} -G {reverse_primer} -o {current_list[2]} -p {current_list[3]} {file.replace(repl_from, repl_to)} {file}'], shell=True) #Run cutadapt in reverse direction

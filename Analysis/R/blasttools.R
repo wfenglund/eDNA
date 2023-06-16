b351938e544238a119e46a05bb280686674985f5
Thomas Källman <thomas.kallman@nrm.se>
2023-06-16 12:40:28 +0200

    Move more blast functionality to package and update package files

--- Analysis/R/blasttools.R
+++ Analysis/R/blasttools.R
@@ -1,7 +1,93 @@
+#' Parse blast output
+#'
+#' This function will create a dataframe with sequence names,
+#' sequences similarity from blast analysis together with taxonomic
+#' information from NCBI. It needs the blast analysis to be locally
+#' available. Check the example file test.out included with the
+#' package to see the requirements of the input file.
+#'
+#' Needs an active internet connection that can reach NCBI to work.
+#' NB! for optimal performance make sure to get a token from NCBI.
+#'
+#' @param dgeList count data in the form of DGEList
+#' @param blastRes file with blast results assumes blastoutput option
+#' -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend
+#' sstart send evalue bitscore staxids sscinames scomnames"
+#'
+#' @importFrom taxize tax_name
+#' @importFrom gtools mixedorder
+#'
+#' @return seqTax a dataframe with annotation of results from sequence
+#' comparisons using blast.
+#'
+#' @export
+#'
+#' @examples
+#' fastqR1 <- system.file("extdata", "exampleFq_R1.fastq.gz",
+#' package = "MetaBAnalysis")
+#' fastqR2 <- system.file("extdata", "exampleFq_R2.fastq.gz",
+#' package = "MetaBAnalysis")
+#' parseTest <- DadaAnalysis(fastqR1, fastqR2, muThread = FALSE)
+#' dfForward <- as.data.frame(t(parseTest))
+#' yForward <- edgeR::DGEList(dfForward)
+#' BlastParse(dgeList = yForward, blastRes = system.file("extdata", "test.out", package = "MetaBAnalysis"))
+
+BlastParse <- function(dgeList, blastRes) {
+  sequences <- data.frame(id = paste("Seq", 1:length(rownames(dgeList)), sep = "_"),
+                          seq = row.names(dgeList))
+  blastRes <- read.table(blastRes, sep = "\t", quote = "'", stringsAsFactors = FALSE)
+  names(blastRes) <- c("qseqid",
+                        "sseqid",
+                        "pident",
+                        "length",
+                        "mismatch",
+                        "gapopen",
+                        "qstart",
+                        "qend",
+                        "sstart",
+                        "send",
+                        "evalue",
+                        "bitscore",
+                        "staxids",
+                        "sscinames",
+                        "scomnames")
+  blastResUn <- blastRes[!duplicated(blastRes$qseqid),] # Retain only
+                                                        # best hits check manually
+  taxonomy <- taxize::tax_name(sci = levels(factor(blastResUn$sscinames)),
+                               get = c("superkingdom",
+                                       "phylum",
+                                       "order",
+                                       "class",
+                                       "family"),
+                               db = "ncbi")
+
+  blastTax <- merge(blastResUn, taxonomy, by.x = "sscinames", by.y = "query", all.x = TRUE)
+  blastTax <- blastTax[,c(2,3,4,12, 1, 17:21)]
+  names(blastTax) <- c("id",
+                        "besthit",
+                        "identity",
+                        "e-value",
+                        "species",
+                        "superkingdom",
+                        "phylum",
+                        "order",
+                        "class",
+                        "family")
+  seqTax <- merge(sequences, blastTax, by.x = "id", by.y = "id", all.x = TRUE)
+  seqTax$id <- as.character(seqTax$id)
+  seqTax <- seqTax[gtools::mixedorder(seqTax$id),]
+  seqTax$superkingdom <- factor(seqTax$superkingdom)
+  seqTax$phylum <- factor(seqTax$phylum)
+  seqTax$order <- factor(seqTax$order)
+  seqTax$class <- factor(seqTax$class)
+  seqTax$family <- factor(seqTax$family)
+  return(seqTax)
+}
+
 #' Blast a given nucleotide sequence at NCBI
 #' 
 #' This facilitate the submission of a nucleotide sequence to NCBIs
-#' blast database. Will fetch XXX blast hits and return these hits as
+#' blast database. Will fetch blast hits and return these hits as
 #' a dataframe. This function largely replaces the need to have a
 #' local blast database to compare to. If you have an active internet
 #' access this method is strongly preferred to any approach relying on
@@ -12,7 +98,7 @@
 #' @param nucleotide string of DNA sequence that should be submitted to
 #' blast
 #'
-#' @return dataframe with XXX of blast hits for @param nucleotide. If
+#' @return dataframe of blast hits for @param nucleotide. If
 #' the sequence is not similar to any known sequence at NCBI this will be
 #' reported and an dataframe with NA values will be returned.
 #' 
@@ -67,4 +153,61 @@ OnlineBlaster <- function(nucleotide) {
   }
   print("- Results retrieved from BLAST.")
   return(result_output)
+}
+
+#' Facilitate blast analysis of all sequences i fasta-file
+#'
+#' Parsing every sequence entry in a fasta file and use the
+#' OnlineBlaster function to blast individual sequences directly on
+#' NCBI.
+#' Needs an active internet connection that can reach NCBI to work.
+#'
+#' @param fastaFile file with fasta sequences
+#' @param seqNumber number of sequences from fasta to analys if set to
+#' 0 all sequences in the file are extracted.
+#' @param resultNumber the number of blast hits per sequence to retain
+#' @param viewChoice if set to yes/Yes/YES a view of the dataframe with blast
+#' results will be shown using the function View.
+#' 
+#' @return blast results for sequences in a dataframe
+#'
+#' @export
+#'
+#' @examples
+#' fastafile <- system.file("extdata", "test.fa", package = "MetaBAnalysis")
+#' MultiBlaster(fastaFile = fastafile)
+
+MultiBlaster <- function(fastaFile, seqNumber = 0, resultNumber = 5, viewChoice = "") { # Function that blasts all sequences in a fasta-file
+  fastaString <- paste(readLines(fastaFile), collapse = "\n")
+  fastaList <- strsplit(fastaString, split = ">")
+  fastaList <- fastaList[[1]][fastaList[[1]] != ""]
+  newLineSplit <- function(inputLine) {
+    sequenceList <- strsplit(inputLine, split = "\n")[[1]]
+    nucSequence <- sequenceList[2:length(sequenceList)]
+    nucSequence <- paste(nucSequence, collapse = "")
+    sequenceList <- c(sequenceList[1], nucSequence)
+    return(sequenceList)
+  }
+  fastaList <- lapply(fastaList, newLineSplit)
+  blastResults <- c()
+  viewResults <- c()
+  if(seqNumber > 0 && length(fastaList) > seqNumber) {
+    fastaList <- fastaList[1:seqNumber]
+  }
+  for(seq in fastaList) {
+    print(paste0("# Sequence ", seq[1], ":"))
+    blastOut <- c()
+    while(length(blastOut) < 2) { # try while in order to try again if http-error gets raised
+      blastOut <- try(OnlineBlaster(seq[2])[1:resultNumber,])
+    }
+    blastOut <- cbind(Sequence = c(seq[1], seq[1], seq[1], seq[1], seq[1]), blastOut)
+    blastResults <- rbind(blastResults, blastOut)
+    
+    if(toupper(viewChoice) == "YES") {
+      viewResults <- rbind(viewResults, blastOut[, -c(2)], c("", "", "", "", "", "", "", "", "", "", "", ""))
+      View(viewResults)
+    }
+    Sys.sleep(2)
+  }
+  return(blastResults)
 }

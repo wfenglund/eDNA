20993200a5e290a97c52984eede60280978791f6
Thomas Källman <thomas.kallman@nrm.se>
2023-11-01 14:33:25 +0100

    Modify the script to use md5sum correctly

--- Scripts/AWSdownload/awsdownload_script.sh
+++ Scripts/AWSdownload/awsdownload_script.sh
@@ -9,7 +9,7 @@ credentials_file=~/.aws/credentials
 # Save current credentials to backup if present:
 touch $credentials_file
 cat $credentials_file >> ~/.aws/credentials.bak
-    #echo "vimrules"
+
 
 # Write new credentials:
 echo "# "${awsinfo[0]} > $credentials_file
@@ -21,8 +21,5 @@ echo "aws_secret_access_key = "${awsinfo[2]} >> $credentials_file
 aws s3 cp s3://${awsinfo[0]} . --recursive
 
 # Check md5sums:
-md5sum */*/Data/*.fq.gz > localmd5sum.txt
-sort -k2 localmd5sum.txt > localmd5sum.txt
-sort -k2 */*/Data/data_md5.txt > bmkmd5sum.txt
-echo "Differences between md5sums (if any):"
-diff localmd5sum.txt bmkmd5sum.txt
+(cd */*/Data/; md5sum --check data_md5.txt)
+
